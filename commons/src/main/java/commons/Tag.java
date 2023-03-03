8ddbb64cd95b46b0152831ae6371ba0982101771
Joey Li <j.w.li@student.tudelft.nl>
2023-03-03 17:57:02 +0100

    tag class

--- commons/src/main/java/commons/Tag.java
+++ commons/src/main/java/commons/Tag.java
@@ -1,4 +1,41 @@
 package commons;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Tag {
+
+    @Id @GeneratedValue
+    private int id;
+
+    private String name;
+    private int colour;
+
+    public Tag() {}
+
+    public Tag(final String name, final int colour) {
+        this.name = name;
+        this.colour = colour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getColour() {
+        return colour;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColour(int colour) {
+        this.colour = colour;
+    }
+
 }
