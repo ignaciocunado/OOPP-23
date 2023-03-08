c176165162b9d8ce3c93f6a3163cf69aec2ea747
Ignacio Cuñado Barral <i.cunadobarral@student.tudelft.nl>
2023-03-08 15:23:40 +0100

    Finished deleteTag, deleteTask, addTag, addTask and their JavaDocs

--- server/src/main/java/server/api/CardController.java
+++ server/src/main/java/server/api/CardController.java
@@ -4,6 +4,7 @@ import commons.Card;
 import commons.Tag;
 import commons.Task;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.CardRepository;
@@ -17,63 +18,105 @@ public class CardController {
     private final Random random;
     private final CardRepository cardRepository;
 
-
+    /**
+     * Constructor for card controller
+     * @param cardService service for application logic
+     * @param random injected object of random class
+     * @param cardRepository repository of cards
+     */
     public CardController(CardService cardService, Random random, CardRepository cardRepository) {
         this.cardService = cardService;
         this.random = random;
         this.cardRepository = cardRepository;
     }
+
     /**
      * to do
      * @param id to do
-     * @return
+     * @param card to do
+     * @return to do
      */
     @PatchMapping("/{id}")
-    public int editCard(@PathVariable int id, @RequestBody Card card) {
+    public ResponseEntity<Card> editCard(@PathVariable final int id, @RequestBody Card card) {
+        if(!cardRepository.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
 
     }
 
     /**
-     * to do
-     * @return to do
+     * creates a Tag and stores it in a Card
+     * @param id id of the Card in which to store the Tag
+     * @param tag the Tag to create and add
+     * @return ResponseEntity for status
      */
     @PostMapping("/{id}/tag")
-    public ResponseEntity<Tag> createTag(@PathVariable int id, @RequestBody Tag tag) {
-        if(tag.getName() != null || tag.getColour() < 0){
+    public ResponseEntity<Card> createTag(@PathVariable final int id, @RequestBody Tag tag) {
+        if(tag.getName() == null || tag.getColour() < 0 || id < 0 ||
+            !cardRepository.existsById(id)) {
             return ResponseEntity.badRequest().build();
         }
-        cardRepository.findById(id).get().addTag(tag);
+        Card containsNewTag = cardRepository.getById(id);
+        containsNewTag.addTag(tag);
+        cardRepository.save(containsNewTag);
+        return new ResponseEntity(cardRepository.getById(id), new HttpHeaders(), 200);
 
     }
 
     /**
-     * to do
-     * @return to do
+     * deletes a Tag if it exists
+     * @param id id of the Card where the Tag to be deleted is
+     * @param tag the Tag to delete
+     * @return ResponseEntity for status
      */
     @DeleteMapping("/{id}/tag")
-    public int deleteTag(@PathVariable int id, ) {
-
-    }
-
-    /**
-     * to do
-     * @return to do
-     */
-    @PostMapping("/{id}/task")
-    public ResponseEntity<Task> createTask(@PathVariable int id, @RequestBody Task task){
-        if(task.getId() < 0 || task.getName() == null) {
+    public ResponseEntity<Card> deleteTag(@PathVariable int id, @RequestBody Tag tag) {
+        if(!cardRepository.existsById(id)) {
             return ResponseEntity.badRequest().build();
         }
-        cardRepository.findById(id).get().addTask(task);
+        Card deleteTagFrom = cardRepository.getById(id);
+        boolean deleted = deleteTagFrom.removeTag(tag);
+        if (!deleted) {
+            return ResponseEntity.badRequest().build();
+        }
+        cardRepository.save(deleteTagFrom);
+        return new ResponseEntity(cardRepository.getById(id), new HttpHeaders(), 200);
     }
 
     /**
-     * to do
-     * @return to do
+     * creates a Task and stores it in a Card
+     * @param id id of the Card in which to store the Task
+     * @param task the Task to create and add
+     * @return ResponseEntity for status
      */
-    @DeleteMapping("/{id}/task")
-    public int deleteTask(@PathVariable int id, ) {
-
+    @PostMapping("/{id}/task")
+    public ResponseEntity<Task> createTask(@PathVariable final int id, @RequestBody Task task){
+        if(task.getId() < 0 || task.getName() == null || id < 0 || cardRepository.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Card containsTask = cardRepository.getById(id);
+        containsTask.addTask(task);
+        cardRepository.save(containsTask);
+        return new ResponseEntity(cardRepository.getById(id), new HttpHeaders(), 200);
     }
 
+    /**
+     * deletes a Task if it exists
+     * @param id id of the Card where the Task to be deleted is
+     * @param task the Task to delete
+     * @return ResponseEntity for status
+     */
+    @DeleteMapping("/{id}/task")
+    public ResponseEntity<Card> deleteTask(@PathVariable final int id, @RequestBody Task task) {
+        if(!cardRepository.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Card deleteTaskFrom = cardRepository.getById(id);
+        boolean deleted = deleteTaskFrom.removeTask(task);
+        if (!deleted) {
+            return ResponseEntity.badRequest().build();
+        }
+        cardRepository.save(deleteTaskFrom);
+        return new ResponseEntity(cardRepository.getById(id), new HttpHeaders(), 200);
+    }
 }
