69ddf87029c3aa8f255bc38be7ca52906faf9a4d
Joey Li <j.w.li@student.tudelft.nl>
2023-03-08 14:36:57 +0100

    Text utils

--- commons/src/main/java/commons/TextUtils.java
+++ commons/src/main/java/commons/TextUtils.java
@@ -1,2 +1,25 @@
-package commons;public class TextUtils {
+package commons;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class TextUtils {
+
+    private static String alphaNumericalSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    /**
+     * Generates a string of alphanumerical string
+     * @param n the length of the randomly generated string
+     * @return random string
+     */
+    public static String randomString(final int n, final String src) {
+        final ThreadLocalRandom random = ThreadLocalRandom.current();
+        return random.ints(n,0,src.length()).mapToObj(number -> String.valueOf(src.charAt(number))).collect(Collectors.joining());
+    }
+
+    public static String randomAlphanumericalString(final int n) {
+        return randomString(n, alphaNumericalSource);
+    }
+
 }
