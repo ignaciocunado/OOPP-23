35e23b33a86aa805bdcc42641bd3508e868567b0
Joey Li <j.w.li@student.tudelft.nl>
2023-03-08 14:37:13 +0100

    Board controller

--- server/src/main/java/server/api/controllers/BoardController.java
+++ server/src/main/java/server/api/controllers/BoardController.java
@@ -13,35 +13,60 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package server.api;
+package server.api.controllers;
 
+import commons.Board;
+import commons.CardList;
 import commons.Quote;
+import commons.TextUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.BoardRepository;
+import server.database.CardListRepository;
 import server.database.QuoteRepository;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Random;
 
 @RestController
-@RequestMapping("/api/boards")
+@RequestMapping("/api/board")
 public class BoardController {
 
-    private final BoardRepository repo;
+    private final BoardRepository boardRepo;
+    private final CardListRepository cardListRepository;
 
     /**
      * RestAPI Controller for the board route
-     * @param repo
+     * @param boardRepo
      */
-    public BoardController(final BoardRepository repo) {
-        this.repo = repo;
+    public BoardController(final BoardRepository boardRepo, final CardListRepository cardListRepository) {
+        this.boardRepo = boardRepo;
+        this.cardListRepository = cardListRepository;
     }
 
     @PostMapping(path = { "", "/" })
-    public List<Quote> createBoard() {
+    public ResponseEntity<Board> createBoard(@RequestBody final Board request) {
+        final String newKey = TextUtils.randomAlphanumericalString(10);
+        final Board board = new Board(newKey, request.getPassword());
+        return new ResponseEntity<>(this.boardRepo.save(board), new HttpHeaders(), 200);
+    }
 
-        return repo.findAll();
+    @GetMapping("/{id}")
+    public ResponseEntity<Board> getBoard(@PathVariable final Integer id) {
+        return new ResponseEntity<>(this.boardRepo.getById(id), new HttpHeaders(), 200);
+    }
+
+    @PostMapping("/{id}/list")
+    public ResponseEntity<Board> createList(@PathVariable final Integer id) {
+        final CardList list = new CardList("New List");
+        this.cardListRepository.save(list);
+
+        final Board board = this.boardRepo.getById(id);
+        board.getListsOnBoard().add(list);
+        this.boardRepo.save(board);
+        return new ResponseEntity<>(this.boardRepo.getById(id), new HttpHeaders(), 200);
     }
 
 }
