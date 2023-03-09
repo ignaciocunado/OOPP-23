47a70f8dc0ab13d5fe05771f511bf382291dc82d
Ignacio Cuñado Barral <i.cunadobarral@student.tudelft.nl>
2023-03-09 15:13:21 +0100

    Created basic tests structure

--- server/src/test/java/server/api/CardControllerTest.java
+++ server/src/test/java/server/api/CardControllerTest.java
@@ -1,6 +1,7 @@
 package server.api;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import server.database.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +23,54 @@ class CardControllerTest {
         controller = new CardController(service, cardRepo,tagRepo,taskRepo);
     }
 
+    @Test
+    public void editCardTitleTest() {
+
+    }
+
+    @Test
+    public void editCardTitleNotFoundTest() {
+
+    }
+
+    @Test
+    public void editCardDescriptionTest() {
+
+    }
+
+    @Test
+    public void editCardDescriptionNotFoundTest() {
+
+    }
+
+    @Test
+    public void createTagTest() {
+
+    }
+
+    @Test
+    public void deleteTagTest() {
+
+    }
+
+    @Test
+    public void deleteTagNotFoundTest() {
+
+    }
+
+    @Test
+    public void createTaskTest() {
+
+    }
 
 
+    @Test
+    public void deleteTaskTest() {
+
+    }
+
+    @Test
+    public void deleteTaskNotFoundTest() {
+
+    }
 }
