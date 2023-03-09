853dc65b7adfa8660ff10cff244dddbabed74a58
Andrea Vezzuto <a.vezzuto@student.tudelft.nl>
2023-03-09 22:43:27 +0100

    switched to providing a new list through Java code rather than using a template, also fixed Javadoc and unused imports

--- client/src/main/java/client/scenes/BoardOverviewCtrl.java
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -21,11 +21,16 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -72,7 +77,7 @@ public class BoardOverviewCtrl implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        hbox.setSpacing(10);
+        hbox.setSpacing(20);
         refresh();
     }
 
@@ -95,20 +100,88 @@ public class BoardOverviewCtrl implements Initializable {
         mainCtrl.minimizeWindow();
     }
 
+    /**
+     * Adds a List to the board
+     */
     public void addList() {
-        try {
+        Pane newList = new Pane();
+        newList.setLayoutX(25);
+        newList.setLayoutY(51);
+        newList.setPrefHeight(500);
+        newList.setPrefWidth(250);
+        newList.setMinHeight(500);
+        newList.setMinWidth(250);
+        newList.setStyle("-fx-background-color: rgb(35,69,103); -fx-border-radius: 15; -fx-background-radius: 15; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.7), 10, 0, 0, 0);");
+
+        Button addButton = new Button();
+        addButton.setContentDisplay(ContentDisplay.CENTER);
+        addButton.setGraphicTextGap(10);
+        addButton.setLayoutX(25);
+        addButton.setLayoutY(65);
+        addButton.setMnemonicParsing(false);
+        addButton.setPrefHeight(22);
+        addButton.setPrefWidth(200);
+        addButton.setMaxWidth(200);
+        addButton.setStyle("-fx-background-color: #123456; -fx-padding: 0px;");
+        addButton.setText("+");
+        addButton.setTextAlignment(TextAlignment.CENTER);
+        addButton.setTextFill(Color.WHITE);
+        Font f = new Font(25);
+        addButton.setFont(f);
+        newList.getChildren().add(addButton);
+
+        Text txt = new Text();
+        txt.setLayoutX(20);
+        txt.setLayoutY(33);
+        txt.setStrokeWidth(0);
+        txt.setText("Title");
+        txt.setWrappingWidth(250);
+        txt.setStrokeType(StrokeType.OUTSIDE);
+        txt.setStyle("-fx-font-size: 30;");
+        txt.setFill(Color.rgb(208,87,87));
+        newList.getChildren().add(txt);
+
+        Button deleteButton = new Button();
+        deleteButton.setContentDisplay(ContentDisplay.CENTER);
+        deleteButton.setAlignment(Pos.CENTER);
+        deleteButton.setLayoutX(215);
+        deleteButton.setLayoutY(-5);
+        deleteButton.setMnemonicParsing(false);
+        deleteButton.setPrefHeight(38);
+        deleteButton.setPrefWidth(31);
+        deleteButton.setMaxWidth(31);
+        deleteButton.setStyle("-fx-background-color: rgb(35,69,103); -fx-label-padding: 0px; -fx-padding: 0px; -fx-border-radius: 15; -fx-background-radius: 15;");
+        deleteButton.setText("-");
+        deleteButton.setTextFill(Color.WHITE);
+        Font fDelete = new Font(38);
+        deleteButton.setFont(fDelete);
+        newList.getChildren().add(deleteButton);
+
+        newList.setId(String.valueOf(hbox.getChildren().indexOf(newList)));
+        hbox.getChildren().add(newList);
+
+        /*try {
             loader = new FXMLLoader(getClass().getResource("ListTemplate.fxml"));
             Pane newList = (Pane)loader.load();
             hbox.getChildren().add(newList);
             //newList.setId(String.valueOf(hbox.getChildren().indexOf(newList)));
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
+
+    /**
+     * Removes a List from the board
+     */
     public void removeList() {
         Pane toBeRemoved = (Pane) hbox.getChildren().get(Integer.parseInt(hoverId));
         hbox.getChildren().remove(toBeRemoved);
     }
+
+    /**
+     * Gets the id of the parent of the element that generated the event
+     * @param event caused by user
+     */
     public void getParentId(Event event){
         Button btn = null;
         if (event.getSource().getClass() == btn.getClass()) {
