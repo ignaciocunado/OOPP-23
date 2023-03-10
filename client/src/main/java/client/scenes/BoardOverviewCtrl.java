abb785d89599791f6ee66ec38110b6d1b49f73e7
Johan Bakker <j.m.bakker-2@student.tudelft.nl>
2023-03-11 00:04:12 +0100

    DRAFT: added addCard()

--- client/src/main/java/client/scenes/BoardOverviewCtrl.java
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -15,45 +15,39 @@
  */
 package client.scenes;
 
+import client.MyFXML;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import javafx.event.Event;
+import commons.Board;
+import commons.Card;
+import commons.CardList;
+import commons.Tag;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.StrokeType;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.ResourceBundle;
 
 public class BoardOverviewCtrl implements Initializable {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
-
-
-//    private ObservableList<Board> data;
-    @FXML
-    private Pane root;
-
     @FXML
     private HBox hbox;
     @FXML
-    private FXMLLoader loader;
+    private VBox vbox;
+    private HashSet<Integer> ids = new HashSet<>();
+    private Board currentBoard;
+    private CardList currentList;
 
-//    @FXML
-//    private ArrayList<Pane> lists;
 
-    private String hoverId;
     /**
      * Constructor to inject necessary classes into the controller
      *
@@ -80,14 +74,21 @@ public class BoardOverviewCtrl implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         hbox.setSpacing(20);
         hbox.setPadding(new Insets(20,20,20,0));
-        refresh();
-
+        refresh(new Board("",""));
     }
 
+
     /**
-     * Stub method for refreshing boards
+     * Stub method for refreshing the Board
+     * @param currentBoard the current Board being displayed
      */
-    public void refresh() {}
+    public void refresh(Board currentBoard) {
+        this.currentBoard = currentBoard;
+    }
+
+    public void refreshList(CardList currentList){
+        this.currentList = currentList;
+    }
 
     /**
      * Method to close the app
@@ -104,125 +105,136 @@ public class BoardOverviewCtrl implements Initializable {
     }
 
     /**
-     * Adds a List to the board
+     * Adds a List to the current Board object and displays it
      */
-    public void addList() {
-        Pane newList = paneCreator();
-
-        Button addButton = addButtonCreator();
-        newList.getChildren().add(addButton);
-
-        Text txt = textCreator();
-        newList.getChildren().add(txt);
-
-        Button deleteButton = delButtonCreator();
-        newList.getChildren().add(deleteButton);
-
-        newList.setId(String.valueOf(hbox.getChildren().indexOf(newList)));
-        hbox.getChildren().add(newList);
-    }
-
-    /**
-     * Creates a new Pane
-     * @return the new Pane with the appropriate attributes
-     */
-    public Pane paneCreator() {
-        Pane newList = new Pane();
-        newList.setLayoutX(25);
-        newList.setLayoutY(51);
-        newList.setPrefHeight(500);
-        newList.setPrefWidth(250);
-        newList.setMinHeight(500);
-        newList.setMinWidth(250);
-        newList.setMaxHeight(550);
-        newList.setStyle("-fx-background-color: rgb(35,69,103); -fx-border-radius: 15; " +
-            "-fx-background-radius: 15; -" +
-            "fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.7), 10, 0, 0, 0);");
-        return newList;
-    }
-
-    /**
-     * Creates a Button to add new Cards
-     * @return the new Button with the appropriate attributes
-     */
-    public Button addButtonCreator() {
-        Button addButton = new Button();
-        addButton.setContentDisplay(ContentDisplay.CENTER);
-        addButton.setGraphicTextGap(10);
-        addButton.setLayoutX(25);
-        addButton.setLayoutY(57);
-        addButton.setMnemonicParsing(false);
-        addButton.setPrefHeight(22);
-        addButton.setPrefWidth(200);
-        addButton.setMaxWidth(200);
-        addButton.setStyle("-fx-background-color: #123456; -fx-padding: 0px;");
-        addButton.setText("+");
-        addButton.setTextAlignment(TextAlignment.CENTER);
-        addButton.setTextFill(Color.WHITE);
-        Font fontAdd = new Font(25);
-        addButton.setFont(fontAdd);
-        return addButton;
-    }
-
-    /**
-     * Creates a new Button to delete a list
-     * @return the new Button with the appropriate attributes
-     */
-    public Button delButtonCreator() {
-        Button deleteButton = new Button();
-        deleteButton.setContentDisplay(ContentDisplay.CENTER);
-        deleteButton.setAlignment(Pos.CENTER);
-        deleteButton.setLayoutX(200);
-        deleteButton.setLayoutY(0);
-        deleteButton.setMnemonicParsing(false);
-        deleteButton.setPrefHeight(30);
-        deleteButton.setPrefWidth(30);
-        deleteButton.setMaxWidth(30);
-        deleteButton.setMaxHeight(30);
-        deleteButton.setStyle("-fx-background-color: rgb(35,69,103); -fx-label-padding: 0px; " +
-            "-fx-padding: 0px; -fx-border-radius: 15; -fx-background-radius: 15;");
-        deleteButton.setText("-");
-        deleteButton.setTextFill(Color.WHITE);
-        Font fontDelete = new Font(38);
-        deleteButton.setFont(fontDelete);
-        return deleteButton;
-    }
-
-    /**
-     * Creates a new Text field for the title of a List
-     * @return the new Text with the appropriate attributes
-     */
-    public Text textCreator() {
-        Text txt = new Text();
-        txt.setLayoutX(22);
-        txt.setLayoutY(35);
-        txt.setStrokeWidth(0);
-        txt.setText("Title");
-        txt.setWrappingWidth(250);
-        txt.setStrokeType(StrokeType.OUTSIDE);
-        txt.setStyle("-fx-font-size: 30;");
-        txt.setFill(Color.rgb(208,87,87));
-        return txt;
-    }
-
-    /**
-     * Removes a List from the board
-     */
-    public void removeList() {
-        Pane toBeRemoved = (Pane) hbox.getChildren().get(Integer.parseInt(hoverId));
-        hbox.getChildren().remove(toBeRemoved);
-    }
-
-    /**
-     * Gets the id of the parent of the element that generated the event
-     * @param event caused by user
-     */
-    public void getParentId(Event event){
-        Button btn = null;
-        if (event.getSource().getClass() == btn.getClass()) {
-            btn = (Button) event.getSource();
-            Pane parent = (Pane) btn.getParent();
-            hoverId = parent.getId();
+    public void addList() throws IOException {
+        Pane listPane = FXMLLoader.load(getLocation("client", "scenes", "ListTemplate.fxml"));
+        hbox.getChildren().add(listPane);
+        int counter = 1;
+        while(ids.contains(counter)){
+            counter++;
         }
+        ids.add(counter);
+        listPane.setId(String.valueOf(counter));
+        CardList currentList = new CardList();
+        for (int i = 0; i < listPane.getChildren().size(); i++) {
+            if (listPane.getChildren().get(i).getClass() == Pane.class) {
+                Pane current = (Pane) listPane.getChildren().get(i);
+                current.getChildren().get(0).setOnMouseClicked(event-> {
+                    try {
+                        addCard();//addCard will be here
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+            if (listPane.getChildren().get(i).getClass() == Button.class) {
+                listPane.getChildren().get(i).setOnMouseClicked(event-> {
+                    removeList(listPane, currentList.getId());
+                });
+            }
+            if (listPane.getChildren().get(i).getClass() == TextField.class) {
+                TextField title = (TextField) listPane.getChildren().get(i);
+                title.setText("Title: "+listPane.getId());
+                refreshTitle(currentList, title);
+                title.setOnKeyReleased(event -> refreshTitle(currentList, title));
+            }
+        }
+        currentBoard.addList(currentList);
+    }
+
+    public void addCard() throws IOException {
+        Pane listPane = FXMLLoader.load(getLocation("client", "scenes", "CardTemplate.fxml"));
+        vbox.getChildren().add(listPane);
+        int counter = 1;
+        while(ids.contains(counter)){
+            counter++;
+        }
+        ids.add(counter);
+        listPane.setId(String.valueOf(counter));
+        Card currentCard = new Card();
+        Pane cardPane = (Pane) listPane.getChildren().get(0);
+        for (int i = 0; i < cardPane.getChildren().size(); i++) {
+            if (cardPane.getChildren().get(i).getClass() == Pane.class) {
+                Pane current = (Pane) cardPane.getChildren().get(i);
+                TextField tag = (TextField) current.getChildren().get(0);
+                tag.setText("Tag");
+                refreshTag(currentCard, tag);
+                tag.setOnKeyReleased(event -> refreshTag(currentCard, tag));
+
+//                current.getChildren().get(0).setOnMouseClicked(event-> {
+//                    try {
+//                        addList();//addCard will be here
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                });
+            }
+            if (listPane.getChildren().get(i).getClass() == Button.class) {
+                listPane.getChildren().get(i).setOnMouseClicked(event-> {
+                    removeCard(cardPane, currentCard.getId());
+                });
+            }
+            if (listPane.getChildren().get(i).getClass() == TextField.class) {
+                TextField cardTitle = (TextField) listPane.getChildren().get(i);
+                cardTitle.setText("Title: "+listPane.getId());
+                refreshCardTitle(currentCard, cardTitle);
+                cardTitle.setOnKeyReleased(event -> refreshCardTitle(currentCard, cardTitle));
+            }
+        }
+        currentList.addCard(currentCard);
+    }
+    /**
+     * Refreshes the title of a List
+     * @param selectedList the selected CardList
+     * @param selectedText the TextFiled associated to the List
+     */
+    public void refreshTitle(CardList selectedList, TextField selectedText) {
+        selectedList.setTitle(selectedText.getText());
+    }
+
+    public void refreshCardTitle(Card selectedCard, TextField selectedText){
+        selectedCard.setTitle(selectedText.getText());
+    }
+    public void refreshTag(Card selectedCard, TextField selectedText){
+        selectedCard.addTag(new Tag(selectedText.getText(), 111));
+    }
+
+    /**
+     * Removes an existing List from the Board
+     * @param toBeRemoved the Pane which needs to be deleted
+     * @param idOfList the ID of the List which needs to be deleted
+     */
+    public void removeList(Pane toBeRemoved, int idOfList) {
+        ids.remove(toBeRemoved.getId());
+        int index = hbox.getChildren().indexOf(toBeRemoved);
+        hbox.getChildren().remove(index);
+        for (CardList list : currentBoard.getListsOnBoard()) {
+            if (list.getId() == idOfList) {
+                currentBoard.removeList(list);
+                break;
+            }
+        }
+    }
+
+    public void removeCard(Pane toBeRemoved, int idOfCard){
+        ids.remove(toBeRemoved.getId());
+        int index = vbox.getChildren().indexOf(toBeRemoved);
+        vbox.getChildren().remove(index);
+        for (Card card : currentList.getCards()){
+            if(card.getId() == idOfCard){
+                currentList.removeCard(card);
+                break;
+            }
+        }
+    }
+    /**
+     * Gets the location of a resource with the given String elements
+     * @param parts Strings of where to find the resource
+     * @return the URL of the requested resource
+     */
+    private URL getLocation(String... parts) {
+        var path = Path.of("", parts).toString();
+        return MyFXML.class.getClassLoader().getResource(path);
     }
 }
