ad75eb60327885725d4f4ce539ce5ebe31a9d6df
Joey Li <j.w.li@student.tudelft.nl>
2023-03-20 22:43:13 +0100

    use field instead of variable name

--- server/src/main/java/server/exceptions/InvalidRequestException.java
+++ server/src/main/java/server/exceptions/InvalidRequestException.java
@@ -2,6 +2,7 @@ package server.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.util.stream.Collectors;
 
@@ -13,7 +14,10 @@ public final class InvalidRequestException extends ServerException {
                 "Invalid request, check reason",
                 errors.getAllErrors()
                         .stream()
-                        .map(error -> error.getObjectName() + " " + error.getDefaultMessage())
+                        // This should be safe as the only annotations are field annotations
+                        .map(error -> (FieldError) error)
+                        .map(error -> error.getField() + " " + error.getDefaultMessage())
                         .collect(Collectors.toList()));
     }
+
 }
