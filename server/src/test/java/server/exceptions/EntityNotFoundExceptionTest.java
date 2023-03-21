84a29cb136bc34f7c2b2cc5711d6a7cde4697dc0
Joey Li <j.w.li@student.tudelft.nl>
2023-03-21 15:01:28 +0100

    invalid request exception tests

--- server/src/test/java/server/exceptions/EntityNotFoundExceptionTest.java
+++ server/src/test/java/server/exceptions/EntityNotFoundExceptionTest.java
@@ -1,12 +1,24 @@
 package server.exceptions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 public final class EntityNotFoundExceptionTest {
 
     @Test
-    public void testEntityNotFoundConstructor() {
+    public void entityNotFoundConstructorTest() {
         new EntityNotFoundException("");
+    }
+
+    @Test
+    public void entityNotFoundGetStatusTest() {
+        Assertions.assertEquals(new EntityNotFoundException("").getStatus(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void entityNotFoundGetReasonTest() {
+        Assertions.assertEquals(new EntityNotFoundException("Good Reason").getReason(), "Good Reason");
     }
 
 }
