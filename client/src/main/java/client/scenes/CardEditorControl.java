86e58c166b2c21dc93b0d0023806ab2804984e38
Ignacio Cuñado Barral <i.cunadobarral@student.tudelft.nl>
2023-03-22 20:55:12 +0100

    Added ids to hbox and vbox and added them in the controller

--- client/src/main/java/client/scenes/CardEditorControl.java
+++ client/src/main/java/client/scenes/CardEditorControl.java
@@ -1,4 +1,15 @@
 package client.scenes;
 
+import commons.entities.Card;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class CardEditorControl {
+
+    @FXML
+    private HBox tags;
+    @FXML
+    private VBox nestedTaskList;
+    private Card currentCard;
 }
