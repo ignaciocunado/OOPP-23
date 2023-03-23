8c3862fbf6f93e8c24489c4807c2996744b94d7d
Ignacio Cuñado Barral <i.cunadobarral@student.tudelft.nl>
2023-03-23 21:57:25 +0100

    Checkstyle fox

--- client/src/main/java/client/scenes/CardOverviewCtrl.java
+++ client/src/main/java/client/scenes/CardOverviewCtrl.java
@@ -1,12 +1,4 @@
 package client.scenes;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class CardOverviewCtrl {
 }
