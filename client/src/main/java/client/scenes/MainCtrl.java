bbb6fbc72334603ffcf8d6bb84ee09c3d8403dab
Joey Li <j.w.li@student.tudelft.nl>
2023-03-26 17:38:14 +0200

    show settings button handler

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -18,6 +18,7 @@ package client.scenes;
 import commons.entities.Board;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Pair;
@@ -25,12 +26,16 @@ import javafx.util.Pair;
 public class MainCtrl {
 
     private Stage primaryStage;
+    private Stage boardSettingsStage;
 
     private LandingOverviewCtrl landingOverviewCtrl;
     private Scene landingOverview;
 
     private BoardOverviewCtrl boardOverviewCtrl;
     private Scene boardOverview;
+
+    private BoardSettingsCtrl boardSettingsCtrl;
+    private Scene boardSettings;
 
     /**
      * Initialize main controller with all FXML controllers
@@ -39,14 +44,22 @@ public class MainCtrl {
      * @param boardOverview the main board overview
      */
     public void initialize(Stage primaryStage, Pair<LandingOverviewCtrl, Parent> landingOverview,
-            Pair<BoardOverviewCtrl, Parent> boardOverview) {
+            Pair<BoardOverviewCtrl, Parent> boardOverview, Pair<BoardSettingsCtrl, Parent> boardSettings) {
         this.primaryStage = primaryStage;
+        this.boardSettingsStage = new Stage();
 
         this.landingOverviewCtrl = landingOverview.getKey();
         this.landingOverview = new Scene(landingOverview.getValue());
 
         this.boardOverviewCtrl = boardOverview.getKey();
         this.boardOverview = new Scene(boardOverview.getValue());
+
+        this.boardSettingsCtrl = boardSettings.getKey();
+        this.boardSettings = new Scene(boardSettings.getValue());
+
+        this.boardSettingsStage.setScene(this.boardSettings);
+        this.boardSettingsStage.initModality(Modality.APPLICATION_MODAL);
+        this.boardSettingsStage.initOwner(this.primaryStage);
 
         primaryStage.initStyle(StageStyle.DECORATED);
         showLandingOverview();
@@ -78,6 +91,14 @@ public class MainCtrl {
         primaryStage.setTitle("Talio: Task List Organiser");
         primaryStage.setScene(this.boardOverview);
         boardOverviewCtrl.refresh(new Board("",""));
+    }
+
+    /**
+     * Shows an existing board overview scene
+     */
+    public void showBoardSettings() {
+        this.boardSettingsStage.setTitle("Talio: Task List Organiser (Settings)");
+        this.boardSettingsStage.showAndWait();
     }
 
     /**
