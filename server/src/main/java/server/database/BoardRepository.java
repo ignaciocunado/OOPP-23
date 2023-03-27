fc9332c424e5c84a974a2c3e68243aaf2104cdaa
Joey Li <j.w.li@student.tudelft.nl>
2023-03-27 14:01:07 +0200

    checkstyle fixes

--- server/src/main/java/server/database/BoardRepository.java
+++ server/src/main/java/server/database/BoardRepository.java
@@ -23,9 +23,19 @@ import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 
+    /**
+     * Gets the board from the repository by a key
+     * @param key the key
+     * @return the board
+     */
     @Query("SELECT b FROM Board b WHERE b.key = ?1")
     Board getBoardByKey(final String key);
 
+    /**
+     * Finds the board from the repository by a key
+     * @param key the key
+     * @return the board
+     */
     @Query("SELECT b FROM Board b WHERE b.key = ?1")
     Optional<Board> findBoardByKey(final String key);
 
