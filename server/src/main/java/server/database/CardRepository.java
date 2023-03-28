3c7da1076f32314bbac68dab885832a1fe36e0d7
Oliwier Szymon Jurek <O.S.Jurek@student.tudelft.nl>
2023-03-28 13:31:03 +0200

    Checkstyle fix

--- server/src/main/java/server/database/CardRepository.java
+++ server/src/main/java/server/database/CardRepository.java
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
 public interface CardRepository extends JpaRepository<Card, Integer> {
+    /**
+     * Query that returns all ids of cards with a given tag
+     * @param tagId an id of a tag
+     * @return List of ids of cards with a given tag
+     */
     @Query("SELECT ct.id FROM Card AS ct JOIN ct.tags tag WHERE tag.id = ?1")
     List<Integer> selectCardsWithTag(final int tagId);
 }
