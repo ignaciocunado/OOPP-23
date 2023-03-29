250c9be1833f3632bdd1ba33c7f497d09753a9e5
Joey Li <j.w.li@student.tudelft.nl>
2023-03-29 18:08:52 +0200

    add service for tags

--- server/src/main/java/server/services/TagService.java
+++ server/src/main/java/server/services/TagService.java
@@ -3,8 +3,6 @@ package server.services;
 import commons.entities.Board;
 import commons.entities.Card;
 import commons.entities.Tag;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import server.database.BoardRepository;
 import server.database.CardRepository;
@@ -15,72 +13,28 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public final class BoardService {
+public final class TagService {
 
-    private final TextService textService;
-    private final BoardRepository boardRepository;
     private final TagRepository tagRepository;
     private final CardRepository cardRepository;
 
-    public BoardService(final TextService textService,
-                        final BoardRepository boardRepository,
-                        final TagRepository tagRepository,
-                        final CardRepository cardRepository) {
-        this.textService = textService;
-        this.boardRepository = boardRepository;
+    public TagService(final TagRepository tagRepository,
+                      final CardRepository cardRepository) {
         this.tagRepository = tagRepository;
         this.cardRepository = cardRepository;
     }
 
-    public Board createBoard(final String password) {
-        final String newKey = this.textService.randomAlphanumericalString(10);
-        final Board board = new Board(newKey, password);
-        return this.boardRepository.save(board);
+    public Tag createTag(final Tag tag) {
+        return this.tagRepository.save(tag);
     }
 
-    public Board getBoard(final String key) {
-        final Optional<Board> boardOpt = this.boardRepository.findBoardByKey(key);
-        if (boardOpt.isEmpty()) {
-            throw new EntityNotFoundException("No board with key " + key);
-        }
-
-        return boardOpt.get();
-    }
-
-    public Board createTag(final int id, final Tag tag) {
-        final Optional<Board> boardOpt = this.boardRepository.findById(id);
-        if (boardOpt.isEmpty()) {
-            throw new EntityNotFoundException("No board with id " + id);
-        }
-
-        final Tag savedTag = this.tagRepository.save(tag);
-        final Board board = boardOpt.get();
-        board.addTag(savedTag);
-        return this.boardRepository.save(board);
-    }
-
-    public Board deleteTag(final int id, final int tagId) {
-        final Optional<Board> boardOpt = this.boardRepository.findById(id);
-        if (boardOpt.isEmpty()){
-            throw new EntityNotFoundException("No board with id " + id);
-        }
-        final Board board = boardOpt.get();
-        if (!board.removeTagById(tagId)) {
-            throw new EntityNotFoundException("No tag with id " + tagId + " in this board");
-        }
-
-        if (cardRepository.count() != 0) {
-            List<Integer> cardsId = cardRepository.selectCardsWithTag(tagId);
-            for (int cardId : cardsId) {
-                Card card = this.cardRepository.getById(cardId);
-                card.removeTagById(tagId);
-                cardRepository.save(card);
-            }
-        }
-
-        this.boardRepository.save(board);
-        this.tagRepository.deleteById(tagId);
-        return board;
+    public void deleteTagFromCards(final int id) {
+        final List<Integer> cardIds = cardRepository.selectCardsWithTag(id);
+        cardIds.forEach(cardId -> {
+            Card card = this.cardRepository.getById(cardId);
+            card.removeTagById(id);
+            this.cardRepository.save(card);
+        });
     }
 
 }
