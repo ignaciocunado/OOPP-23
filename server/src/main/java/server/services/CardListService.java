2b42669143fa663c918490e14f24d72a3b49435c
Joey Li <j.w.li@student.tudelft.nl>
2023-03-29 18:08:28 +0200

    add service for card lists

--- server/src/main/java/server/services/CardListService.java
+++ server/src/main/java/server/services/CardListService.java
@@ -1,23 +1,21 @@
 package server.services;
 
 import commons.entities.Card;
-import commons.entities.Tag;
+import commons.entities.CardList;
 import org.springframework.stereotype.Service;
+import server.database.CardListRepository;
 import server.database.CardRepository;
-import server.database.TagRepository;
-
-import java.util.List;
 
 @Service
-public final class CardService {
-    private final CardRepository cardRepository;
+public final class CardListService {
+    private final CardListRepository cardListRepository;
 
-    public CardService(final CardRepository cardRepository) {
-        this.cardRepository = cardRepository;
+    public CardListService(final CardListRepository cardListRepository) {
+        this.cardListRepository = cardListRepository;
     }
 
-    public Card createCard(final Card card) {
-        return this.cardRepository.save(card);
+    public CardList createCardList(final CardList cardList) {
+        return this.cardListRepository.save(cardList);
     }
 
 }
