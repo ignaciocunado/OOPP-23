f8d6122e94cb93fb5063f55a7a18d32d99d597c2
Andrea Vezzuto <a.vezzuto@student.tudelft.nl>
2023-03-30 11:28:04 +0200

    fix for issue #53

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -61,7 +61,7 @@ public class MainCtrl {
         cardEditorStage.initModality(Modality.APPLICATION_MODAL);
         cardEditorStage.setTitle("Card Editor");
         cardEditorStage.setScene(cardEditorScene);
-        cardEditorScene.getStylesheets().add(getClass().getResource("comboBox.css")
+        cardEditorScene.getStylesheets().add(getClass().getResource("assets/style/comboBox.css")
             .toExternalForm());
 
         primaryStage.initStyle(StageStyle.DECORATED);
