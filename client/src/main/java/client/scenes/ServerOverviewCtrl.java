d9e0a139613a53f459b04340b9964944140f4eec
Joey Li <j.w.li@student.tudelft.nl>
2023-03-30 23:00:54 +0200

    add server overview controller

--- client/src/main/java/client/scenes/ServerOverviewCtrl.java
+++ client/src/main/java/client/scenes/ServerOverviewCtrl.java
@@ -1,2 +1,30 @@
-package client.scenes;public class ServerOverviewCtrl {
+package client.scenes;
+
+import client.config.Config;
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public final class ServerOverviewCtrl {
+
+    @FXML
+    private TextField connectionUriField;
+
+    private final MainCtrl ctrl;
+    private final ServerUtils utils;
+    private final Config config;
+    @Inject
+    public ServerOverviewCtrl(final MainCtrl ctrl, final ServerUtils utils, final Config config) {
+        this.ctrl = ctrl;
+        this.utils = utils;
+        this.config = config;
+    }
+
+    public void connect() {
+        this.config.setCurrentWorkspace(this.connectionUriField.getText());
+        this.utils.setServer(this.connectionUriField.getText());
+        this.ctrl.showLandingOverview();
+    }
+
 }
