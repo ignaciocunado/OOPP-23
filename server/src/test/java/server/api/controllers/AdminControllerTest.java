0fc9f26c89eec378654dd849258eab6297221812
Joey Li <j.w.li@student.tudelft.nl>
2023-04-07 17:44:13 +0200

    admincontroller tests

--- server/src/test/java/server/api/controllers/AdminControllerTest.java
+++ server/src/test/java/server/api/controllers/AdminControllerTest.java
@@ -1,6 +1,8 @@
 package server.api.controllers;
 
 import commons.entities.Board;
+import commons.entities.Card;
+import commons.entities.CardList;
 import commons.entities.Tag;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -71,6 +73,29 @@ public final class AdminControllerTest {
         final Board board = new Board("aaaaaaaaaa","name", "password");
         board.setId(1);
         Assertions.assertEquals(this.boardRepo.findAll(), this.adminController.getAllBoards().getBody());
+    }
+
+    @Test
+    public void getAllBoardsUpdatesAddBoardNestedRelationsTest() throws InterruptedException, ExecutionException {
+        final CompletableFuture<String> fut = new CompletableFuture<>();
+        final DeferredResult<ResponseEntity<List<Board>>> result =
+                this.adminController.getAllBoardsUpdates();
+
+        final Board boardWithRelations = this.boardRepo.save(new Board("", "Test", ""));
+        final Card card = this.cardRepo.save(new Card("Title", "Description"));
+        final CardList list = new CardList("Title");
+        list.addCard(card);
+        this.cardListRepo.save(list);
+        boardWithRelations.addList(list);
+
+        final Board board = this.boardController.createBoard(new Board("", "Test", "")).getBody();
+        result.setResultHandler(resultHandler -> {
+            final ResponseEntity<List<Board>> b = (ResponseEntity<List<Board>>) resultHandler;
+            Assertions.assertEquals(b.getBody().get(0), boardWithRelations);
+            Assertions.assertEquals(b.getBody().get(1), board);
+            fut.complete("done");
+        });
+        fut.get();
     }
 
     @Test
