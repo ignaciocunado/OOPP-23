3eab6f62262eac5b61350f0219c3f03d3f6c5523
Joey Li <j.w.li@student.tudelft.nl>
2023-04-11 23:16:19 +0200

    password is randomly generated in the admin interceptor

--- server/src/main/java/server/InterceptorConfig.java
+++ server/src/main/java/server/InterceptorConfig.java
@@ -5,9 +5,20 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import server.api.interceptors.AdminInterceptor;
 import server.api.interceptors.GeneralInterceptor;
+import server.services.TextService;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final TextService textService;
+
+    /**
+     * Constructor to get the text service for generating the password
+     * @param textService the text service
+     */
+    public InterceptorConfig(final TextService textService) {
+        this.textService = textService;
+    }
 
     /**
      * Adds an interceptor
@@ -16,7 +27,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new GeneralInterceptor()).addPathPatterns("/**");
-        registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/api/admin/**");
+        registry.addInterceptor(new AdminInterceptor(this.textService)).addPathPatterns("/api/admin/**");
     }
 
 }
--- server/src/main/java/server/api/interceptors/AdminInterceptor.java
+++ server/src/main/java/server/api/interceptors/AdminInterceptor.java
@@ -2,13 +2,31 @@ package server.api.interceptors;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import server.exceptions.UnauthorisedException;
+import server.services.TextService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdminInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = Logger.getLogger("Admins");
+    private final TextService textService;
+    private final String password;
+
+    /**
+     * Constructor to get the text service for generating the password
+     * @param textService the text service
+     */
+    public AdminInterceptor(final TextService textService) {
+        this.textService = textService;
+        this.password = this.textService.randomAlphanumericalString(10);
+
+        this.logger.log(Level.INFO, "The current admin password is: "+this.password);
+    }
 
     /**
      * Handles admin requests to authorise and reject if necessary
@@ -33,7 +51,7 @@ public class AdminInterceptor implements HandlerInterceptor {
         String password = parts[1];
 
         // validate username and password against a user store or authentication provider
-        if (!"admin".equals(username) || !"password".equals(password)) {
+        if (!"admin".equals(username) || !this.password.equals(password)) {
             throw new UnauthorisedException("Invalid username or password");
         }
         return true;
