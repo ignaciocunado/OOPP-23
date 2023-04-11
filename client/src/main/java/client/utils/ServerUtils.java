0f800951188de6ac15bdb39362176aec4a294595
Ignacio Cuñado Barral <i.cunadobarral@student.tudelft.nl>
2023-04-11 16:26:53 +0200

    Checkstyle

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -173,8 +173,9 @@ public class ServerUtils {
                     .request(MediaType.APPLICATION_JSON)
                     .accept(MediaType.APPLICATION_JSON)
                     .method(HttpMethod.PATCH,
-                            Entity.json(new Board(board.getKey(),
-                                    board.getName(), board.getPassword(), board.getColour(), board.getFontColour())),
+                            Entity.json(new Board(board.getKey(), board.getName(),
+                                    board.getPassword(), board.getColour(),
+                                    board.getFontColour())),
                             Board.class);
         } catch (NotFoundException e) {
             return null;
