51d00b4c08870c1f6ff420a71c942218876d2a3a
Joey Li <j.w.li@student.tudelft.nl>
2023-04-11 21:10:10 +0200

    fix wrong method

--- server/src/main/java/server/api/controllers/PingController.java
+++ server/src/main/java/server/api/controllers/PingController.java
@@ -2,7 +2,7 @@ package server.api.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class PingController {
      *
      * @return request to signify server status
      */
-    @PostMapping(path = {"", "/"})
+    @GetMapping(path = {"", "/"})
     public ResponseEntity<String> ping() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
